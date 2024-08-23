bfa06ba5deb300c944aa8987626d98522222878f
Ale <ale.aranda@duocuc.cl>
2024-08-23 16:09:25 -0400

    inicio proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pasteleria',
+  webDir: 'www'
+};
+
+export default config;
