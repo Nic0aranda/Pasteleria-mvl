54d4e8762e75aa2391cdc3ac241c49f66eb787e9
Ale <ale.aranda@duocuc.cl>
2024-09-02 19:41:54 -0400

    envio de datos correcto

---
+++ src/app/pages/vendedor/inicio-v/inicio-v.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio-v',
+  templateUrl: './inicio-v.page.html',
+  styleUrls: ['./inicio-v.page.scss'],
+})
+export class InicioVPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
